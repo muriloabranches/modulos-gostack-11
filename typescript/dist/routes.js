110f17c91b141168704a94f5d19338773005aec4
Murilo Abranches <muriloads@hotmail.com>
2020-04-12 13:37:03 -0300

    Add TypeScript module

---
+++ typescript/dist/routes.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var CreateUser_1 = __importDefault(require("./services/CreateUser"));
+function helloWorld(request, response) {
+    var user = CreateUser_1.default({
+        email: 'murilo@teste.com',
+        password: 123456,
+        techs: [
+            'Node.js',
+            'ReactJS',
+            'React Native',
+            { title: 'JavaScript', experience: 100 }
+        ]
+    });
+    return response.json(user);
+}
+exports.helloWorld = helloWorld;
